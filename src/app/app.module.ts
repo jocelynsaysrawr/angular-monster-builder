69621f35d9d39019ded8b47d2236dfddc2b4dc84
Jocelyn Belden <jocebelden@gmail.com>
2018-04-26 21:08:46 -1000

    add basic file tree

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeadsComponent } from './navigation/heads/heads.component';
+import { ArmsComponent } from './navigation/arms/arms.component';
+import { BodyComponent } from './navigation/body/body.component';
+import { LegsComponent } from './navigation/legs/legs.component';
+import { DisplayComponent } from './display/display.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HeadsComponent,
+    ArmsComponent,
+    BodyComponent,
+    LegsComponent,
+    DisplayComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navigation/arms/arms.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArmsComponent } from './arms.component';
+
+describe('ArmsComponent', () => {
+  let component: ArmsComponent;
+  let fixture: ComponentFixture<ArmsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ArmsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArmsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
