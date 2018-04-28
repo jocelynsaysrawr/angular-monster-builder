52825a48c0dadb798fca9aaa929e72721fcad532
Steven Inouye <steveninouye@msn.com>
2018-04-28 12:07:18 -1000

    added list of heads

--- src/app/navigation/heads/heads.component.ts
+++ src/app/navigation/heads/heads.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heads.component.scss']
 })
 export class HeadsComponent implements OnInit {
+  allHeads = ['Big Head', 'Small Head', 'Little Head', 'Fat Head'];
 
   constructor() { }
 
