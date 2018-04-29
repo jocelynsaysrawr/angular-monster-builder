22c837e5982f6b041c7cd5ea8ce380b2d1d642cc
Jocelyn Belden <jocebelden@gmail.com>
2018-04-28 14:37:15 -1000

    add dropdown feature to side nav

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from "@angular/core";
 })
 export class NavigationComponent implements OnInit {
   dropdownStatus: boolean = false;
+  activeButton: string = "";
 
   constructor() {}
 
   ngOnInit() {}
 
-  onShowNavigation() {
+  onShowNavigation(buttonName: string) {
     this.dropdownStatus = this.dropdownStatus ? false : true;
+    this.activeButton = buttonName;
+
+    console.log("activeButton: ", this.activeButton);
   }
 }
