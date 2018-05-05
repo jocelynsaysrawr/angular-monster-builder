db64189e710bc09a2e8e00ce78c943ae0e2a2b32
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 12:45:02 -1000

    changed port to 8080

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bp = require("body-parser");
 var cors = require("cors");
 const knex = require("./knex/knex.js");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
