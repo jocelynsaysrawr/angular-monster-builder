54c8db5f22b17905de4c17793b5bcb55139ce5f2
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 09:39:21 -1000

    mecha shiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { HeadsComponent } from "./display/heads/heads.component";
-import { ArmsComponent } from "./display/arms/arms.component";
+import { LeftArmComponent } from "./display/arms/left-arm/left-arm.component";
+import { RightArmComponent } from "./display/arms/right-arm/right-arm.component";
 import { BodyComponent } from "./display/body/body.component";
 import { LegsComponent } from "./display/legs/legs.component";
 import { DisplayComponent } from "./display/display.component";
@@ -19,7 +20,8 @@ import { BodyService } from "./body.service";
     HeaderComponent,
     NavigationComponent,
     HeadsComponent,
-    ArmsComponent,
+    LeftArmComponent,
+    RightArmComponent,
     BodyComponent,
     LegsComponent,
     DisplayComponent
