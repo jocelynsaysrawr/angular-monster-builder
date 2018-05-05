5eefddb4db4b3fd33e60b6fe626163c488553e4b
Mark W <markwuu@gmail.com>
2018-05-05 10:19:54 -1000

    imported animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
