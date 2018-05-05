fbba2536d751d7e972facf134d91dacecbffaa00
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 11:20:50 -1000

    minor cleaning

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { HeadService } from "../head.service";
-import { ArmService } from "../arm.service";
 import { BodyService } from "../body.service";
 
 @Component({
@@ -10,22 +9,16 @@ import { BodyService } from "../body.service";
 })
 export class DisplayComponent implements OnInit {
   selectedHead: Object;
-  selectedArms: Object;
   constructor(
     public headService: HeadService,
-    public armService: ArmService,
     public bodyService: BodyService
   ) {
     this.selectedHead = headService.selectedHead;
-    this.selectedArms = armService.selectedArms;
   }
 
   ngOnInit() {
     this.headService.head$.subscribe(head => {
       this.selectedHead = head;
-    });
-    this.armService.arm$.subscribe(arm => {
-      this.selectedArms = arm;
     });
   }
 }
