b4b677cf714ebbd859ec467c3e8663c91ac4912b
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 19:55:43 -1000

    savey thingy

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,8 +1,10 @@
-Component({
+import { Component, OnInit } from "@angular/core";
+
+@Component({
   selector: "app-display",
   templateUrl: "./display.component.html",
   styleUrls: ["./display.component.scss"]
-});
+})
 export class DisplayComponent implements OnInit {
   constructor() {}
 
