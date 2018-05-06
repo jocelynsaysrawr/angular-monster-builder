5fdc2cba25eba74ede6f79093bef01fb58f1e559
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 17:56:40 -1000

    updated users table and user_monsters relationship table

--- server/knex/seeds/users.js
+++ server/knex/seeds/users.js
@@ -1,74 +1,79 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users')
+  return knex("users")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('users')
+      return knex("users")
         .insert([
           {
-            username: 'jimkim',
-            password: 'password'
+            username: "jimkim",
+            password: "password"
           },
           {
-            username: 'jesseleung',
-            password: 'password'
+            username: "jesseleung",
+            password: "password"
           },
           {
-            username: 'markwuu',
-            password: 'password'
+            username: "markwuu",
+            password: "password"
           },
           {
-            username: 'jocelynbelden',
-            password: 'password'
+            username: "jocelynbelden",
+            password: "password"
           },
           {
-            username: 'steveninouye',
-            password: 'password'
+            username: "steveninouye",
+            password: "password"
           },
           {
-            username: 'admin',
-            password: 'password'
+            username: "admin",
+            password: "password"
           }
         ])
         .then(() => {
-          return knex('user_monsters')
+          return knex("user_monsters")
             .del()
             .then(function() {
-              return knex('user_monsters').insert([
+              return knex("user_monsters").insert([
                 {
                   user_id: 1,
                   head_id: 2,
                   body_id: 2,
-                  arm_id: 1,
+                  left_arm_id: 1,
+                  right_arm_id: 1,
                   leg_id: 1
                 },
                 {
                   user_id: 2,
                   head_id: 1,
                   body_id: 1,
-                  arm_id: 1,
+                  left_arm_id: 1,
+                  right_arm_id: 1,
                   leg_id: 1
                 },
                 {
                   user_id: 3,
                   head_id: 2,
                   body_id: 2,
-                  arm_id: 2,
+                  left_arm_id: 2,
+                  right_arm_id: 2,
                   leg_id: 2
                 },
                 {
                   user_id: 4,
                   head_id: 3,
                   body_id: 3,
-                  arm_id: 3,
+                  left_arm_id: 3,
+                  right_arm_id: 3,
                   leg_id: 3
                 },
                 {
                   user_id: 5,
                   head_id: 1,
                   body_id: 2,
-                  arm_id: 3,
+                  left_arm_id: 3,
+                  right_arm_id: 3,
                   leg_id: 2
                 }
               ]);
--- user_monsters.js
+++ user_monsters.js
@@ -1,43 +1,48 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('user_monsters')
+  return knex("user_monsters")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('user_monsters').insert([
+      return knex("user_monsters").insert([
         {
           user_id: 1,
           head_id: 2,
           body_id: 2,
-          arm_id: 1,
+          left_arm_id: 1,
+          right_arm_id: 1,
           leg_id: 1
         },
         {
           user_id: 2,
           head_id: 1,
           body_id: 1,
-          arm_id: 1,
+          left_arm_id: 1,
+          right_arm_id: 1,
           leg_id: 1
         },
         {
           user_id: 3,
           head_id: 2,
           body_id: 2,
-          arm_id: 2,
+          left_arm_id: 2,
+          right_arm_id: 2,
           leg_id: 2
         },
         {
           user_id: 4,
           head_id: 3,
           body_id: 3,
-          arm_id: 3,
+          left_arm_id: 3,
+          right_arm_id: 3,
           leg_id: 3
         },
         {
           user_id: 5,
           head_id: 1,
           body_id: 2,
-          arm_id: 3,
+          left_arm_id: 3,
+          right_arm_id: 3,
           leg_id: 2
         }
       ]);
