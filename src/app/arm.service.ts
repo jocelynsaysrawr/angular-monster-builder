ceace5ae0ccb11989720040fdc4c66abcf1fec93
Jocelyn Belden <jocebelden@gmail.com>
2018-05-05 15:58:03 -1000

    add arms to assets

--- src/app/arm.service.ts
+++ src/app/arm.service.ts
@@ -8,42 +8,32 @@ export class ArmService {
     {
       id: 0,
       name: "Hulk Arm",
-      LArmSrc:
-        "https://previews.123rf.com/images/lineartestpilot/lineartestpilot1404/lineartestpilot140404348/27861342-flexing-monster-arm-cartoon.jpg",
-      RArmSrc:
-        "https://previews.123rf.com/images/lineartestpilot/lineartestpilot1404/lineartestpilot140404348/27861342-flexing-monster-arm-cartoon.jpg"
+      LArmSrc: "./assets/arms/left-hulk-arm.png",
+      RArmSrc: "./assets/arms/right-hulk-arm.png"
     },
     {
       id: 1,
       name: "Werewolf Arm",
-      LArmSrc:
-        "https://thumbs.dreamstime.com/z/halloween-monster-arm-creative-design-55369559.jpg",
-      RArmSrc:
-        "https://thumbs.dreamstime.com/z/halloween-monster-arm-creative-design-55369559.jpg"
+      LArmSrc: "./assets/arms/left-orange-arm.png",
+      RArmSrc: "./assets/arms/right-orange-arm.png"
     },
     {
       id: 2,
       name: "Floopy Arm",
-      LArmSrc:
-        "https://img.clipartxtras.com/46a44eff1e6c0ae5d4db65fc85495e56_vector-of-cartoon-monster-arm-csp19392081-search-clip-art-monster-arms-clip-art_431-470.jpeg",
-      RArmSrc:
-        "https://img.clipartxtras.com/46a44eff1e6c0ae5d4db65fc85495e56_vector-of-cartoon-monster-arm-csp19392081-search-clip-art-monster-arms-clip-art_431-470.jpeg"
+      LArmSrc: "./assets/arms/left-floopy-arm.png",
+      RArmSrc: "./assets/arms/right-floopy-arm.png"
     },
     {
       id: 3,
       name: "Golem Arm",
-      LArmSrc:
-        "http://blog-imgs-31.fc2.com/u/n/i/uniuni0216/20100503173426df9.png",
-      RArmSrc:
-        "http://blog-imgs-31.fc2.com/u/n/i/uniuni0216/20100503173426df9.png"
+      LArmSrc: "./assets/arms/left-golem-arm.png",
+      RArmSrc: "./assets/arms/right-golem-arm.png"
     },
     {
       id: 4,
       name: "Tentacle Arm",
-      LArmSrc:
-        "http://www.livescience.com/images/i/000/055/385/original/octopus-tentacle.jpg",
-      RArmSrc:
-        "http://www.livescience.com/images/i/000/055/385/original/octopus-tentacle.jpg"
+      LArmSrc: "./assets/arms/left-tentacle-arm.png",
+      RArmSrc: "./assets/arms/right-tentale-arm.png"
     }
   ];
 
