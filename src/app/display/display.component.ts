323f1f11a46d6ffa05c866cad50acc5b4bb6764c
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-05-05 19:46:16 -1000

    clean display component

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,38 +1,10 @@
-<<<<<<< HEAD
-import { Component, OnInit } from "@angular/core";
-import { HeadService } from "../head.service";
-import { BodyService } from "../body.service";
-=======
-import { Component, OnInit } from '@angular/core';
-import { IHead } from '../models/head.model';
-import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
->>>>>>> auth
-
-@Component({
-  selector: 'app-display',
-  templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
-})
+Component({
+  selector: "app-display",
+  templateUrl: "./display.component.html",
+  styleUrls: ["./display.component.scss"]
+});
 export class DisplayComponent implements OnInit {
-<<<<<<< HEAD
-  selectedHead: Object;
-  constructor(
-    public headService: HeadService,
-    public bodyService: BodyService
-  ) {
-    this.selectedHead = headService.selectedHead;
-  }
-
-  ngOnInit() {
-    this.headService.head$.subscribe(head => {
-      this.selectedHead = head;
-    });
-  }
-=======
-  selectedArms: Object;
   constructor() {}
 
   ngOnInit() {}
->>>>>>> auth
 }
