c3013dd338afb1fbb405725041c5ed5904c203ec
Steven Inouye <steveninouye@msn.com>
2018-05-07 23:29:48 -1000

    working on registration route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bp = require("body-parser");
-var cors = require("cors");
-const knex = require("./knex/knex.js");
+const express = require('express');
+const bp = require('body-parser');
+var cors = require('cors');
+const knex = require('./knex/knex.js');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,42 +9,52 @@ app.use(cors());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: false }));
 
-app.get("/head", (req, res) => {
+app.post('/register', (req, res) => {
+  let user = req.body;
+  knex
+    .insert(user)
+    .into('users')
+    .then(data => {
+      res.send(data);
+    });
+});
+
+app.get('/head', (req, res) => {
   knex
     .select()
-    .from("heads")
+    .from('heads')
     .then(data => {
       res.json(data);
     });
 });
-app.get("/left-arm", (req, res) => {
+app.get('/left-arm', (req, res) => {
   knex
     .select()
-    .from("left_arms")
+    .from('left_arms')
     .then(data => {
       res.json(data);
     });
 });
-app.get("/right-arm", (req, res) => {
+app.get('/right-arm', (req, res) => {
   knex
     .select()
-    .from("right_arms")
+    .from('right_arms')
     .then(data => {
       res.json(data);
     });
 });
-app.get("/leg", (req, res) => {
+app.get('/leg', (req, res) => {
   knex
     .select()
-    .from("legs")
+    .from('legs')
     .then(data => {
       res.json(data);
     });
 });
-app.get("/body", (req, res) => {
+app.get('/body', (req, res) => {
   knex
     .select()
-    .from("bodys")
+    .from('bodys')
     .then(data => {
       res.json(data);
     });
